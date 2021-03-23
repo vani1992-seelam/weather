eee57849c9967dec560534b78df7adb354ecb9e7
Bhaavani <bhaavani.reddy@gmail.com>
2021-03-23 16:34:15 -0400

    54-Refactoring from functionl to class components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        err => console.log(err)
-    );
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            err => console.log(err)
+        );
 
-    return <div>Hello There!!</div>;
 
-};
+        return <div>Latitude: </div>;
+    }
+}
 
 ReactDom.render(<App />, document.querySelector('#root'));
