4de90f538d14b078b362d64f2a904f628e3327c5
Bhaavani <bhaavani.reddy@gmail.com>
2021-03-25 17:22:15 -0400

    60-Error handling method in proper way

--- src/index.js
+++ src/index.js
@@ -5,20 +5,27 @@ class App extends React.Component {
     constructor(props) {
         super(props);
 
-        this.state = { lat: null };
+        this.state = { lat: null, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 this.setState({ lat: position.coords.latitude });
             },
-            err => console.log(err)
+            err => {
+                this.setState({ errorMessage: err.message});
+            }
         );
-
-
     }
-    render() {
-        
-        return <div>Latitude: {this.state.lat}</div>;
+
+
+    render() {        
+        return (
+            <div>
+                Latitude: {this.state.lat}
+                <br />
+                Error: {this.state.errorMessage}
+            </div>
+        );
     }
 }
 
