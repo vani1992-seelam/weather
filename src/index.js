97bfc83a9c4474f93b7044068e4451cd15c81bb5
Bhaavani <bhaavani.reddy@gmail.com>
2021-03-25 16:26:52 -0400

    57-Intializing State Through Constructors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { lat: null };
+    }
     render() {
         window.navigator.geolocation.getCurrentPosition(
             position => console.log(position),
